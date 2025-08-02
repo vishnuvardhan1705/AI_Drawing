f5d80fca5c4f0f042cf1d0b5718977d6991522fb
Vishnu Vardhan Reddy Mannuru <vishnuvardhanmannuru@gmail.com>
2025-08-02 23:47:24 +0530

    created admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import drawpage,drawsteps
 
-admin.site.register(drawpage)
-admin.site.register(drawsteps)
+
+@admin.register(drawpage)
+class drawpageAdmin(admin.ModelAdmin):
+    list_display=('id','name','description')
+
+@admin.register(drawsteps)
+class drawstepsAdmin(admin.ModelAdmin):
+    list_display=('id','stepcontent','stepno','image','steptext')
+    list_filter=('stepcontent',)
+
 
