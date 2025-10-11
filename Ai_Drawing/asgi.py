9921c1f83d4541bb8b26ca8015d79817ba651f09
Vishnu Vardhan Reddy Mannuru <vishnuvardhanmannuru@gmail.com>
2025-10-11 20:45:07 +0530

    Fix Render path issue

--- Ai_Drawing/asgi.py
+++ Ai_Drawing/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AI_Drawing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ai_Drawing.settings')
 
 application = get_asgi_application()
