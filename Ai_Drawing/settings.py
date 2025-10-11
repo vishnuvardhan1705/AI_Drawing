1daa612d1262aa8c1436ce858ad367da6482eefb
Vishnu Vardhan Reddy Mannuru <vishnuvardhanmannuru@gmail.com>
2025-10-11 19:55:15 +0530

    prepare for Render deployment

--- Ai_Drawing/settings.py
+++ Ai_Drawing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mbkt$x8v#+8jml^fh-%%7on7q=a&r)1n&u!x+67+jht3r-_m1i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'requirements.txt'
 ]
 
 MIDDLEWARE = [
